a903cdb308727246c7c705773f4de626fc43a5cd
sumitmern123 <117528598+sumitmern123@users.noreply.github.com>
2022-12-28 15:39:27 +0530

    Add files via upload

---
+++ AuthButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+const AuthButton=props=>{
+    let { isLoggedIn}=props;
+    if(isLoggedIn){
+        return <button> Logout</button>
+    }
+    else{
+        return <button> Login</button>
+    }
+}
+export default AuthButton;
